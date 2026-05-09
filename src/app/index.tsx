53c22f034f157eb55db6b814c6a43b78640c0f49
Ansh Deep <ad0811@srmist.edu.in>
2026-05-09 17:01:15 +0530

    add initial files

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,130 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, StyleSheet, Text, View ,Image, TextInput, Pressable, KeyboardAvoidingView, Platform,} from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { Ionicons } from "@expo/vector-icons";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+const index = () => {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+    <ScrollView style={{flex: 1}}>
+      <View style={{padding:16,alignItems: 'center'}}>
+        <Image
+        source={require('@/assets/images/logo2-preview.png')}
+        style={{
+          height: 40,
+          width: 40,
+          margin: 15
+        }}
+      />
+      <Text style={{fontSize: 32,fontWeight:'bold'}}>Sign In</Text>
+      <Text style={{color: 'grey'}}>Let's experience the joy of telecare AI</Text>
+      </View>
+      <KeyboardAvoidingView 
+      style={{flex:1}}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+      <View style={{padding: 16}}>
+        <Text style={{fontWeight: 'bold'}}>Email Address</Text>
+
+        <View style={{
+          flexDirection:'row',
+          alignItems:'center',
+          borderWidth: 1,
+          borderColor: "#ddd",
+          borderRadius: 20,
+          marginTop: 2,
+          padding: 6,
+          marginBottom: 12,}}>
+        <Icon name="envelope" size={15} style={{marginLeft: 8, marginRight: 6}}/>
+        <TextInput 
+        placeholder='elementary221b@gmail.com' 
+        >
+        </TextInput>
+        </View>
+        <Text style={{fontWeight: 'bold'}}>Password</Text>
+        <View 
+        style={{
+          flexDirection:'row',
+          alignItems:'center',
+          borderWidth: 1,
+          borderColor: "#ddd",
+          borderRadius: 20,
+          marginTop: 2,
+          padding: 6,
+          marginBottom: 12,}}
+        >
+          <Icon name="lock" size={20} style={{marginLeft: 8, marginRight: 6}} />
+        <TextInput 
+        placeholder='Enter your password...' 
+        secureTextEntry
+        style={{
+      flex: 1,
+      marginLeft: 2
+    }}
+        >
+        </TextInput>
+        <Ionicons name="eye-off-outline" size={24} color="#bdb8b8"/>
+        </View>
+        <Pressable
+        style={{
+          backgroundColor: "#8DB600",
+          paddingVertical: 16,
+          borderRadius: 18,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection:'row',
+          gap: 8
+        }}
+        >
+        <Text style={{color: 'white',fontWeight:'bold',fontSize:14,}}>Sign In 
+        </Text>
+        <Ionicons name="arrow-forward" size={20} color="white" />
+        </Pressable>
+      </View>
+      </KeyboardAvoidingView>
+
+      <View style={{padding:16,justifyContent:'center',flexDirection:'row',gap: 8}}>
+        <Pressable
+          style={{
+            padding: 12,
+            borderWidth: 1,
+            borderColor: "#d3cfcf",
+            borderRadius: 14,
+          }}
+        >
+        <Icon name="facebook" size={24} color="#030405" />
+        </Pressable>
+        <Pressable
+          style={{
+            padding: 12,
+            borderWidth: 1,
+            borderColor: "#d3cfcf",
+            borderRadius: 14,
+          }}
+        >
+        <Icon name="google" size={24} color="#030405" />
+        </Pressable>
+        <Pressable
+          style={{
+            padding: 12,
+            borderWidth: 1,
+            borderColor: "#d3cfcf",
+            borderRadius: 14,
+          }}
+        >
+        <Icon name="instagram" size={24} color="#030405" />
+        </Pressable>
+      </View>
+
+      <View style={{padding:16,alignItems:'center'}}>
+        <Text>Don't have an account? <Text style={{color: "#8DB600",textDecorationLine: 'underline'}}>Sign up</Text> </Text>
+        <Text style={{color:"#8DB600", textDecorationLine: 'underline'}}>Forgot your password?</Text>
+      </View>
+      
+    </ScrollView>
+  )
 }
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+export default index
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
+const styles = StyleSheet.create({})
